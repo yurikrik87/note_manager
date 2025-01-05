0890568fbdaa0e6c75326e318018449010bef165
yurikrik87 <yuri_st_87@mail.ru>
2025-01-05 21:56:55 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Юрий'
+title = 'Поход к стамотологу'
+content = 'Пломбировка зуба'
+status = 'Активна'
+created_date = '26-01-2025' # дата создания заметки
+issue_date = '26-05-2025' # дата истечения заметки
+print('Имя ползователя:',username)
+print('Заголовок заметки:',title)
+print('Описание заметки:',content)
+print('Статус заметки:',status)
+print('Дата начала:',created_date)
+print('Дата завершения:',issue_date)
