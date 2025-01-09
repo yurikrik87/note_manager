e04a898b396bd953d4ff9a256315a9e98aa42f72
yurikrik87 <yuri_st_87@mail.ru>
2025-01-09 22:01:59 +0300

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,5 @@
+title_1 = input('Введите заголовок заметки 1: ')
+title_2 = input('Введите заголовок заметки 2: ')
+title_3 = input('Введите заголовок заметки 3: ')
+title_0= [title_1, title_2, title_3]
+print(title_0)
